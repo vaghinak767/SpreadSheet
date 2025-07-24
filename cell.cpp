9cacb9a9c939415075e9329c76170ba924cc17b9
vaghinakpetrosian <147193790+vaghinakpetrosian@users.noreply.github.com>
2025-07-24 23:00:53 +0400

    Update cell.cpp

--- cell.cpp
+++ cell.cpp
@@ -1,181 +1,12 @@
 #include "cell.h"
+#include <sstream>
 
-Cell::Cell()
+std::ostream& operator<<(std::ostream& out, const std::vector<int>& obj)
 {
-    m_val = "\0";
-}
-
-Cell::Cell(const Cell& obj) : m_val{obj.m_val}
-{}
-
-Cell::Cell(Cell&& rhv) noexcept : m_val{rhv.m_val}
-{
-    rhv.m_val = "\0";
-}
-
-Cell::Cell(int val)
-{
-    m_val = std::to_string(val);
-}
-
-Cell::Cell(double val)
-{
-    m_val = std::to_string(val);
-}
-
-Cell::Cell(std::string val)
-{
-    m_val = val;
-}
-
-Cell::Cell(char val)
-{
-    m_val = std::string(1, val);
-}
-
-Cell::Cell(bool val)
-{
-    m_val = val? "true" : "false";
-}
-
-Cell::Cell(const std::vector<int>& val)
-{
-    m_val = '[';
-    int size = val.size();
-    if(size != 0)
-        m_val += val[0];
-    for(int i = 1; i < size; ++i)
+    for(auto elem : obj)
     {
-        m_val += ',' + val[i];
+        out << elem << " ";
     }
-    m_val += ']';
-}
-
-const Cell& Cell::operator=(const Cell& rhv)
-{
-    m_val = rhv.m_val;
-    return *this;
-}
-
-const Cell& Cell::operator=(Cell&& rhv) noexcept
-{
-    m_val = rhv.m_val;
-    rhv.m_val = "\0";
-    return *this;
-}
-
-const Cell& Cell::operator=(int val)
-{
-    m_val = std::to_string(val);
-    return *this;
-}
-
-const Cell& Cell::operator=(double val)
-{
-    m_val = std::to_string(val);
-    return *this;
-}
-
-const Cell& Cell::operator=(std::string val)
-{
-    m_val = val;
-    return *this;
-}
-
-const Cell& Cell::operator=(char val)
-{
-    m_val = std::string(1, val);
-    return *this;
-}
-
-const Cell& Cell::operator=(bool val)
-{
-    m_val = val? "true" : "false";
-    return *this;
-}
-
-const Cell& Cell::operator=(const std::vector<int>& rhv)
-{
-    m_val = '[';
-    int size = rhv.size();
-    if(size != 0)
-        m_val += rhv[0];
-    for(int i = 1; i < size; ++i)
-    {
-        m_val += ',' + rhv[i];
-    }
-    m_val += ']';
-    return *this;
-}
-
-Cell::operator int()
-{
-    return std::stoi(m_val);
-}
-
-Cell::operator double()
-{
-    return std::stod(m_val);
-}
-
-Cell::operator std::string()
-{
-    return m_val;
-}
-
-Cell::operator char()
-{
-    return m_val[0];
-}
-
-Cell::operator bool()
-{
-    int tmp = std::stoi(m_val);
-    return m_val == "true";
-}
-
-Cell::operator std::vector<int>()
-{
-    size_t size = m_val.size();
-    std::vector<int> vec;
-    int i = 1;
-    while(m_val[i] != '\0' && i < size)
-    {
-        while(m_val[i] != ',' && m_val[i] != ']' && m_val[i] != '\0')
-        {
-            vec.push_back(m_val[i]);
-            ++i;
-        }
-        ++i;
-    }
-    return vec;
-}
-
-std::string Cell::get_str()const
-{
-    return m_val;
-}
-
-std::istream& operator>>(std::istream& in, Cell& str)
-{
-    in >> str.m_val;
-    return in;
-}
-
-bool operator==(const Cell& lhs, const Cell &rhs)
-{
-    return lhs.get_str() == rhs.get_str();
-}
-
-bool operator!=(const Cell& lhs, const Cell &rhs)
-{
-    return !(lhs == rhs);
-}
-
-
-std::ostream& operator<<(std::ostream& out, const Cell& obj)
-{
-    out << obj.get_str() << std::endl;
     return out;
 }
 
@@ -190,11 +21,243 @@ std::istream& operator>>(std::istream& in, std::vector<int>& obj)
     return in;
 }
 
-std::ostream& operator<<(std::ostream& out, const std::vector<int>& obj)
+std::istream& operator>>(std::istream& in, Cell& obj)
 {
-    for(auto elem : obj)
-    {
-        out << elem << " ";
-    }
+    in >> obj.m_val;
+    return in; 
+}
+
+std::ostream& operator<<(std::ostream& out, const Cell& obj)
+{
+    out << obj.get_str();
     return out;
+}
+
+Cell::Cell()
+{
+    m_val = "";
+}
+
+
+Cell::Cell(const Cell& rhv) : m_val{rhv.m_val}{}
+
+Cell::Cell(Cell&& rhv) noexcept: m_val{rhv.m_val} 
+{
+    rhv.m_val = "";
+}
+
+Cell::Cell(std::string val) : m_val{val}{}
+
+Cell::Cell(int val)
+{
+    m_val = std::to_string(val);
+}
+
+Cell::Cell(double val)
+{
+    m_val = std::to_string(val);	   
+}
+
+Cell::Cell(char val) : m_val{val}{}
+
+Cell::Cell(bool val)
+{
+    m_val = val ? "true" : "false";
+}
+
+Cell::Cell(const std::vector<int>& val)
+{
+    size_t i = 0;
+    size_t size = val.size();
+    for(auto elem : val)
+    {
+        if(i == 0)
+        {
+            m_val = '[' + std::to_string(val[i]) + ", ";
+        }
+        else if(i == size - 1)
+        {
+            m_val += std::to_string(val[i]) + ']';
+        }
+        else
+        {
+            m_val += ' ' + std::to_string(val[i]) + ", ";
+        }
+        ++i;
+    } 
+}
+
+const Cell& Cell::operator=(const Cell& rhv)
+{
+    if(this == &rhv)
+    {
+        return *this;
+    }
+    
+    m_val = rhv.m_val;
+    return *this;
+}
+
+
+const Cell& Cell::operator=(Cell&& rhv) noexcept
+{
+    if(this == &rhv)
+    {
+        return *this;
+    }
+
+    m_val = rhv.m_val;
+    rhv.m_val = "";
+    return *this;
+}
+
+const Cell& Cell::operator=(int val)
+{
+    m_val = std::to_string(val);
+    return *this;
+}
+
+const Cell&  Cell::operator=(double val)
+{
+    m_val = std::to_string(val);
+    return *this;
+}
+
+const Cell&  Cell::operator=(std::string val)
+{
+    m_val = val;
+    return *this;
+}
+
+const Cell& Cell::operator=(char val)
+{
+    m_val = val;
+    return *this;
+}
+
+const Cell& Cell::operator=(bool val)
+{
+    m_val = val ? "true" : "false";
+    return *this;
+}  
+
+const Cell& Cell::operator=(const std::vector<int>& val)
+{
+    size_t i = 0;
+    size_t size = val.size();
+    for(auto elem : val)
+    {
+        if(i == 0)
+        {
+            m_val = '[' + std::to_string(val[i]) + ",";
+        }
+        else if(i == size - 1)
+        {
+            m_val += std::to_string(val[i]) + ']';
+        }
+        else
+        {
+            m_val += std::to_string(val[i]) + ",";
+        }
+        ++i;
+    } 
+    return *this;
+}
+Cell::operator std::string()
+{
+    return m_val;
+}
+
+Cell::operator int()
+{
+    int num = 1;
+    std::stringstream info;
+    info << m_val;    
+    info >> num;
+
+    return num;
+}
+
+Cell::operator double()
+{
+    double num = 1;
+    std::stringstream info;
+    info << m_val;    
+    info >> num;
+
+    return num;   
+}
+
+Cell::operator char()
+{
+    return m_val[0];
+}
+
+Cell::operator bool()
+{
+    if(m_val == "True" || m_val == "true")
+    {
+        return true;
+    }
+
+    std::stringstream info;
+    int tmp = 0;
+    info << m_val;
+    info >> tmp;
+    if(tmp != 0)
+    {
+        return true;
+    }
+	return false;
+}
+
+Cell::operator std::vector<int>()
+{
+    std::vector<int> arr;
+    std::string str = "";
+    if(m_val[0] == '[')
+    {
+        for(size_t i = 0; m_val[i] != '\0'; ++i)
+        {
+            if(m_val[i] == '[')
+            {
+                continue;
+            }
+            while(m_val[i] != ',' && m_val[i] != ']')
+            {
+                str += m_val[i];
+                ++i;
+            }
+            int num = 1;
+            std::stringstream info;
+            info << str;    
+            info >> num;
+            arr.push_back(num);
+            str = "";
+        }
+    }
+    else if(m_val[0] >= '0' && m_val[0] <= '9')
+    {
+        int num = 1;
+        std::stringstream info;
+        info << m_val;    
+        info >> num;
+        arr.push_back(num);
+    }
+    return arr;
+}
+
+std::string Cell::get_str()const
+{
+    return m_val;
+}
+
+bool operator==(const Cell& lhs, const Cell &rhs)
+{    
+    return lhs.get_str() == rhs.get_str();
+}
+
+bool operator!=(const Cell& lhs, const Cell &rhs)
+{    
+    return lhs.get_str() != rhs.get_str();
 }
